0476ff6977f33e9ac8fb71114c1513eb9dd8a3f5
aAg3 <80839591+aAg3@users.noreply.github.com>
2023-04-06 02:12:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 348708372 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    o = np.sqrt(np.log(1 + x.var() / x.mean() ** 2)) 
-    m = np.log(x.mean() / np.sqrt(1 + x.var() / x.mean() ** 2)) 
-
-    return np.exp(m - o ** 2 / 2) - 107
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return np.log(x - 107).mean()
