3e12379f97d33bae33f58c7d60475bab502990eb
aAg3 <80839591+aAg3@users.noreply.github.com>
2023-04-05 18:33:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 348708372 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    o = np.sqrt(np.log(1 + x.var() / x.mean() ** 2)) 
+    m = np.log(x.mean() / np.sqrt(1 + x.var() / x.mean() ** 2)) 
+
+    return np.exp(m - o ** 2 / 2) - 107
